9c2d42a2426449a8ae257d95ef9eb12c8ed56866
Wawi <122799427+wawi-m@users.noreply.github.com>
2024-10-03 05:46:34 +0300

    Create 1-promise.js

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+// 1-promise.js
+function getFullResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+        if (success) {
+            resolve({
+                status: 200,
+                body: 'Success',
+            });
+        } else {
+            reject(new Error('The fake API is not working currently'));
+        }
+    });
+}
+
+export default getFullResponseFromAPI;
