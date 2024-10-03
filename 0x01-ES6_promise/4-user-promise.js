bd6efbaca6707c2d9f974d41ba4ac7d644ca287d
Wawi <122799427+wawi-m@users.noreply.github.com>
2024-10-03 05:49:34 +0300

    Create 4-user-promise.js

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,9 @@
+// 4-user-promise.js
+function signUpUser(firstName, lastName) {
+    return Promise.resolve({
+        firstName: firstName,
+        lastName: lastName,
+    });
+}
+
+export default signUpUser;
