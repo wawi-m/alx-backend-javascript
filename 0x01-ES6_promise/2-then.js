72f15dd41c29afe1d4ba436fa37095d0ce261642
Wawi <122799427+wawi-m@users.noreply.github.com>
2024-10-03 05:47:53 +0300

    Create 2-then.js

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,16 @@
+// 2-then.js
+function handleResponseFromAPI(promise) {
+    return promise
+        .then(() => {
+            console.log("Got a response from the API");
+            return {
+                status: 200,
+                body: 'success',
+            };
+        })
+        .catch(() => {
+            return new Error();
+        });
+}
+
+export default handleResponseFromAPI;
