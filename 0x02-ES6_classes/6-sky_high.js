57c0730c8903dea05c08f80cab78a4cd95ea36e5
Wawi <122799427+wawi-m@users.noreply.github.com>
2024-10-04 03:58:32 +0300

    Create 6-sky_high.js

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,25 @@
+import Building from './5-building.js';
+
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft); // Call the parent constructor to initialize sqft
+    this.floors = floors; // Calls the setter for validation
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Floors must be a number');
+    }
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
+
+export default SkyHighBuilding;
