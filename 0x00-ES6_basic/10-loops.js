7cb5b1acf5f0514846142f17313659ec70029821
wawi <wawigladys@gmail.com>
2024-10-04 06:13:36 +0300

    fix lint

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (var idx in array) {
-      var value = array[idx];
-      array[idx] = appendString + value;
-    }
-  
-    return array;
+  for (const idx in array) {
+    const value = array[idx];
+    array[idx] = appendString + value;
   }
+
+  return array;
+}
