8e10510387eb231bff961fb1b0ade6a046013a11
Wawi <122799427+wawi-m@users.noreply.github.com>
2024-10-04 03:57:55 +0300

    Create 5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+class Building {
+  constructor(sqft) {
+    this.sqft = sqft; // Calls the setter for validation
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Square footage must be a number');
+    }
+    this._sqft = value;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
+
+export default Building;
