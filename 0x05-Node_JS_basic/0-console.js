87e387bd57b98651ac1fd4b29ca874ed5b7002fa
wawi <wawigladys@gmail.com>
2025-02-13 23:11:23 +0300

    create tasks 0-8

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Gladys Mwangi <https://github.com/wawi-m>
+ */
+const displayMessage = (msg) => {
+    console.log(msg);
+  };
+  
+  module.exports = displayMessage;
