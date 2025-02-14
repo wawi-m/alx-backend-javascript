9c6b7cd42b84ecf970cdea80d01abfe9ef324941
Wawi <122799427+wawi-m@users.noreply.github.com>
2025-02-14 07:43:45 +0300

    Update 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,10 @@
 /**
  * Displays a message on the STDOUT.
  * @param {String} msg The message to display.
- * @author Gladys Mwangi <https://github.com/wawi-m>
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
  */
 const displayMessage = (msg) => {
-    console.log(msg);
-  };
-  
-  module.exports = displayMessage;
+  console.log(msg);
+};
+
+module.exports = displayMessage;
