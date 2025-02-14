0b2c50dde11cf04b5dca76f34ae978f0d529c334
Wawi <122799427+wawi-m@users.noreply.github.com>
2025-02-14 08:53:13 +0300

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,16 +1,3 @@
-// 1-promise.test.js
-import getFullResponseFromAPI from './1-promise';
-
-describe('getFullResponseFromAPI', () => {
-    it('should resolve with success message when success is true', async () => {
-        const response = await getFullResponseFromAPI(true);
-        expect(response).toEqual({
-            status: 200,
-            body: 'Success',
-        });
-    });
-
-    it('should reject with an error message when success is false', async () => {
-        await expect(getFullResponseFromAPI(false)).rejects.toThrow('The fake API is not working currently');
-    });
-});
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
+}
