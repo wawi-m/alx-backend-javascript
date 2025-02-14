54a30bc63206618ea00707321643ccb12136cf02
Wawi <122799427+wawi-m@users.noreply.github.com>
2025-02-14 08:36:58 +0300

    Create 6-set.js

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Gladys Mwangi <https://github.com/wawi-m>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
